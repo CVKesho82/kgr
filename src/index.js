4438f0a74a5dde79fb726003754bb07842d6cdd9
Carmen <miss.cdv@gmail.com>
2022-07-25 08:39:01 -0400

    render

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
 <React.StrictMode>
  <App />
  </React.StrictMode>
+);
+ 
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
