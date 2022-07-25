6d3cc1a80a27068038abc9e33c7ca37f80cf8c20
Carmen <miss.cdv@gmail.com>
2022-07-25 11:38:40 -0400

    hash

--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
 
+ const history = createBrowserHistory();
  const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
-<React.StrictMode>
+<React.StrictMode history ={history}>
  <App />
- </React.StrictMode>
- 
+ </React.StrictMode> 
 );
- 
- 
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
