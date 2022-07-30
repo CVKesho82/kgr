9f0e226a842eaa5cd5a5a3cd1a689f1edf03afa0
Carmen <miss.cdv@gmail.com>
2022-07-30 19:58:23 -0400

    404

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import AboutUs from '../src/Components/AboutUs';
 import lroom from './Assets/images/livingroom.jpg';
 import NavBar from './Components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route, Switch, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Switch, Link} from 'react-router-dom';
 import Footer from './Components/Footer';
 import Contact from './Components/Contact';
 import Buckhead from './Components/Buckhead';
 import WestEnd from './Components/WestEnd';
 import Events from './Components/Events';
 import Home from './Components/Home';
-import {HashRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
     <Header/>
     <br/>
     <NavBar/> 
-    <BrowserRouter>  
+    <Router>  
         <Routes>
           <Route exact path='/*' element={<Home />} />
           <Route exact path='AboutUs/*' element={<AboutUs/>}/>
@@ -30,7 +30,7 @@ function App() {
           <Route exact path='Events/*' element={<Events/>}/>
           <Route exact path='Contact/*' element={<Contact/>}/>
           </Routes>
-    </BrowserRouter>
+    </Router>
     <br/>                
     <Footer/>
     </div>
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {createBrowserHistory} from 'history';
 const history = createBrowserHistory();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<React.StrictMode>
+<React.StrictMode basename= {process.env.PUBLIC_URL}>
 <App />
 </React.StrictMode> 
 );
