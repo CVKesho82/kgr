320e8e65fabaf4e59604786645e197df2b01fcc4
Carmen <miss.cdv@gmail.com>
2022-07-30 22:34:43 -0400

    history

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<React.StrictMode basename= {process.env.PUBLIC_URL}>
+<React.StrictMode basename= {process.env.PUBLIC_URL} history={history}>
 <App />
 </React.StrictMode>  
 );
+
+
 
 
 // If you want to start measuring performance in your app, pass a function
