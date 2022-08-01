758f4e4b254d3b5b907bc406f971afb03b1550c5
Carmen <miss.cdv@gmail.com>
2022-08-01 07:15:18 -0400

    icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Card} from 'react-bootstrap';
-import {FaRegCopyright, FaFacebook, FaInstagramSquare, FaAirbnb} from "react-icons/fa";
+import {FaRegCopyright, FaFacebook, FaInstagramSquare, FaTwitter} from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer1" >
-    <Card className="footer2"body><FaRegCopyright/> 2022 Kesho Group, LLC 
-    <FaFacebook/><FaInstagramSquare/><FaAirbnb/></Card>
+    <Card className="footer2" body><FaRegCopyright/> 2022 Kesho Group, LLC {' '} 
+    <a href='https://www.facebook.com/keshogroup'><FaFacebook size={30}/></a>{' '}<a href='https://www.instagram.com/keshogrouprentals/'><FaInstagramSquare size={30}/></a>{' '}<a href ='https://twitter.com/KeshoGroupREI'><FaTwitter size={30}/></a>{' '}</Card>
 </div>
   )
 }
