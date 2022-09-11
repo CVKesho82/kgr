9a4543c0032d0afaecd1d6b386fe40602eb973c2
Carmen <miss.cdv@gmail.com>
2022-09-10 20:36:55 -0400

    removed facebook

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className="footer1" >
     <Card className="footer2" body><FaRegCopyright/> 2022 Kesho Group, LLC {' '} 
-    <a href='https://www.facebook.com/keshogroup'><FaFacebook size={30}/></a>{' '}<a href='https://www.instagram.com/keshogrouprentals/'><FaInstagramSquare size={30}/></a>{' '}<a href ='https://twitter.com/KeshoGroupREI'><FaTwitter size={30}/></a>{' '}</Card>
+   <a href='https://www.instagram.com/keshogrouprentals/'><FaInstagramSquare size={30}/></a>{' '}<a href ='https://twitter.com/KeshoGroupREI'><FaTwitter size={30}/></a>{' '}</Card>
 </div>
   )
 }
