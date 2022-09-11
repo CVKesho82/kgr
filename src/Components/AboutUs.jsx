9aa89aec469a0e5f089daa5ea97c40d5de1678c9
Carmen <miss.cdv@gmail.com>
2022-09-10 21:00:05 -0400

    aboutuscardupdate

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,40 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card,Col,CardText,CardTitle,Row, CardBody, CardImage} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 
 function AboutUs() {
   return (
-    <div className="about">
-    <br></br>
-    <div>
-    
-    <Card className="mb-3" style={{ maxWidth: '540px' }}>
-  <Row className="g-0">
-    <Col md={4}>
-      <CardImage src="/images/react400.jpg" />
-    </Col>
-    <Col md={8}>
-      <CardBody>
-        <CardTitle>Kesho Group</CardTitle>
-        <CardText>
-        The Kesho Group, a real estate investment company started in 2012, focuses on offering quality housing for all. <br></br>
+     <Card style={{ width: '18rem' }}>
+        <Card.Img variant="left" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Kesho Group</Card.Title>
+          <Card.Text>
+          A real estate investment company started in 2012, the Kesho Group, focuses on offering quality housing for all.
+          Chris Kesho, owner of the Kesho Group, is an electrical engineer and has been applying his unique technical perspective to the real estate industry for the last 10 years.
 
-        Chris Kesho, owner of the Kesho Group, is an electrical engineer and has been applying his unique technical perspective to the real estate industry for more than 10 years. <br></br>
-        <br></br>
-        </CardText>
-        <CardText>
-          <small className="text-medium-emphasis">Last updated 3 mins ago</small>
-        </CardText>
-      </CardBody>
-    </Col>
-  </Row>
-</Card>
-
- 
-    <br></br>     
-    </div> 
-    </div>
-  )
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
 }
 
 export default AboutUs
