c74087d2edfa478f60649012010472df7f076713
Carmen <miss.cdv@gmail.com>
2022-09-11 22:24:31 -0400

    changedlayoutaboutus

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -6,12 +6,13 @@ import Chris from '../Assets/images/chris.jpg';
 function AboutUs() {
   return (
     <Container className="about">
-        <div className="tile">
-        <Image src = {Chris} width="50%"></Image>
-        </div>
+       
         <div className="tile">
           A real estate investment company started in 2012, the Kesho Group, focuses on offering quality housing for all. <br></br> <br></br>
           Chris Kesho, owner of the Kesho Group, is an electrical engineer and has been applying his unique technical perspective to the real estate industry for more than 10 years.
+        </div>
+        <div className="tile">
+        <Image src = {Chris} width="55%"></Image>
         </div>
   </Container>
   
