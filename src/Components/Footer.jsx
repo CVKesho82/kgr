4ebd47acfaf4634d4e0ec47efd2b9e64ac0559d5
ckesho <ckesho@gmail.com>
2022-12-18 16:24:00 -0500

    formatting and changed foot icon color to red

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
-import {FaRegCopyright, FaFacebook, FaInstagramSquare, FaTwitter} from "react-icons/fa";
+import { Card } from 'react-bootstrap';
+import { FaRegCopyright, FaFacebook, FaInstagramSquare, FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer1" >
-    <Card className="footer2" body><FaRegCopyright/> 2022 Kesho Group, LLC {' '} 
-   <a href='https://www.instagram.com/keshogrouprentals/'><FaInstagramSquare size={30}/></a>{' '}<a href ='https://twitter.com/KeshoGroupREI'><FaTwitter size={30}/></a>{' '}</Card>
-</div>
+      <Card className="footer2" body><FaRegCopyright /> 2022 Kesho Group, LLC {' '}
+        <a href='https://www.instagram.com/keshogrouprentals/'><FaInstagramSquare size={30} color={'#a1211f'} /></a>{' '}<a href='https://twitter.com/KeshoGroupREI'><FaTwitter size={30} color={'#a1211f'} /></a>{' '}</Card>
+    </div>
   )
 }
 export default Footer
