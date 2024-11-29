2ad855279cb538e4eb12cabe159e8d5351d22d10
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2024-11-29 08:29:48 -0500

    Update AboutUs.jsx

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -11,9 +11,9 @@ function AboutUs() {
           <h2>Kesho Group, LLC</h2><br></br>
           A real estate investment company started in 2012, the Kesho Group, focuses on offering quality housing for all. <br></br> <br></br>
           </div>
-        /*{/* <div className="tile"> */}
-        <Image src = {Chris} width ="220px" height="325px" padding-right="0px"></Image>
-        {/* </div> */}*/
+        {/* <div className="tile"> */}
+        /*<Image src = {Chris} width ="220px" height="325px" padding-right="0px"></Image>*/
+        {/* </div> */}
   </Container>
   
   );
