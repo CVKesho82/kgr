bee872c6146b9f397307bada659c62f7e0820b5b
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2024-11-29 08:26:04 -0500

    Update AboutUs.jsx

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -10,7 +10,7 @@ function AboutUs() {
         <div className="tile">
           <h2>Kesho Group, LLC</h2><br></br>
           A real estate investment company started in 2012, the Kesho Group, focuses on offering quality housing for all. <br></br> <br></br>
-          Chris Kesho, owner of the Kesho Group, is an electrical engineer and has been applying his unique technical perspective to the real estate industry for more than 10 years.
+          /*Chris Kesho, owner of the Kesho Group, is an electrical engineer and has been applying his unique technical perspective to the real estate industry for more than 10 years.*/
         </div>
         /*{/* <div className="tile"> */}
         <Image src = {Chris} width ="220px" height="325px" padding-right="0px"></Image>
