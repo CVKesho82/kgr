aa17f6e45b47257f09fff6db5d02d2cc05925e88
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2024-11-29 08:27:40 -0500

    Update AboutUs.jsx

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Image} from 'react-bootstrap';
-import Chris from '../Assets/images/chris.jpg';
+/*import Chris from '../Assets/images/chris.jpg';*/
 
 function AboutUs() {
   return (
